be010b9eb14990cd752fddad3aaaef641fa9cec7
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2024-02-11 23:21:19 +0100

    first

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
-// Define a route for the GET request
-app.get('/', (req, res) => {
-  // Send the "Hello" string as the response
-  res.send('Hello');
+//
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.use(express.json());
+
+app.get('/timer', authenticateUser, function (request, response) {
+  response.sendFile(path.resolve(__dirname, './public/pages', 'timer.html'));
 });
 
 // Specify the port to listen on
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Start the server and listen on the specified port
-
 const start = async () => {
   try {
     app.listen(port, () => {
